86f03cf0f9127109746b0bd262858d8020cbbc0e
zuoeririmo <weiyang007@yeah.net>
2018-07-21 17:18:36 +0800

    开发简单CRUD

--- python/app.py
+++ python/app.py
@@ -5,6 +5,41 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources=r'/*')
 
+@app.route('/modifyUser/<id>', methods=['POST'])
+def modifyUser(id):
+    print(request, '===request')
+    username = request.form['username']
+    conn = mysql.connector.connect(user='root', password='', database='test')
+    cursor = conn.cursor()
+    cursor.execute('update user set name = %s where id = %s', (username, int(id),))
+    print('rowcount=', cursor.rowcount)
+    conn.commit()
+    cursor.close()
+    return userList()
+
+@app.route('/delUser/<id>', methods=['GET'])
+def delUser(id):
+    print(id, '===id')
+    conn = mysql.connector.connect(user='root', password='', database='test')
+    cursor = conn.cursor()
+    cursor.execute('delete from user where id = %s', (int(id),))
+    print('rowcount=', cursor.rowcount)
+    conn.commit()
+    cursor.close()
+    return userList()
+
+@app.route('/addUser', methods=['POST'])
+def addUser():
+    conn = mysql.connector.connect(user='root', password='', database='test')
+    cursor = conn.cursor()
+    id = request.form['id']
+    username = request.form['name']
+    cursor.execute('insert into user (id, name) values (%s, %s)', [id, username])
+    print('rowcount=', cursor.rowcount)
+    conn.commit()
+    cursor.close()
+    return userList()
+
 @app.route('/userList', methods=['GET'])
 def userList():
     conn = mysql.connector.connect(user='root', password='', database='test')
@@ -17,5 +52,6 @@ def userList():
         'users': values
     }
     return jsonify(response)
+
 if __name__ == '__main__':
     app.run(debug=True)
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
-  <div id="app">
-    <el-table
-      :data="users"
-      border
-      stripe
-      style="width: 100%">
-      <el-table-column
-        prop="id"
-        label="编号"
-        >
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        >
-      </el-table-column>
-    </el-table>
+  <div id="app" class="container">
+    <div class="operation">
+      <el-button type="primary" @click="showAddModal">添加</el-button>
+    </div>
+    <div class="custom-grid">
+      <el-table
+        :data="users"
+        border
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="id"
+          label="编号"
+          >
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          >
+        </el-table-column>
+        <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+          <template slot-scope="scope">
+            <el-button @click="showModifyModal(scope.row)" type="text" size="small">修改</el-button>
+            <el-button @click="del(scope.row)" type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <el-dialog :title="title" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="编号" :label-width="formLabelWidth">
+          <el-input v-model="form.id" auto-complete="off" :disabled="idDisabled"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -25,10 +55,132 @@
     name: 'App',
     data() {
       return {
+        idDisabled: false,
+        title: '添加',
         users: [],
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          id: '',
+        },
+        formLabelWidth: '120px'
       }
     },
     methods: {
+      showAddModal() {
+        this.idDisabled = false
+        this.title = '添加'
+        this.form.id = ''
+        this.form.name = ''
+        this.dialogFormVisible = true
+      },
+      showModifyModal(row) {
+        this.idDisabled = true
+        this.title = '修改'
+        this.form.id = row.id
+        this.form.name = row.name
+        this.dialogFormVisible = true
+      },
+      del(row) {
+         axios({
+            url: `http://127.0.0.1:5000/delUser/${row.id}`,
+            method: 'get',
+            transformRequest: [function (data) {
+              // Do whatever you want to transform the data
+              let ret = ''
+              for (let it in data) {
+                ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+              }
+              return ret
+            }],
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          })
+          .then((res) => {
+            this.dialogFormVisible = false
+            this.users = res.data.users.map((user) => {
+                return {
+                  id: user[0],
+                  name: user[1]
+                }
+              })
+          }, (rej) => {
+            this.dialogFormVisible = false
+            console.log(rej)
+            })
+      },
+      add() {
+        axios({
+          url: 'http://127.0.0.1:5000/addUser',
+          method: 'post',
+          data: {
+            ...this.form
+          },
+          transformRequest: [function (data) {
+            // Do whatever you want to transform the data
+            let ret = ''
+            for (let it in data) {
+              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            return ret
+          }],
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then((res) => {
+          this.dialogFormVisible = false
+          this.users = res.data.users.map((user) => {
+              return {
+                id: user[0],
+                name: user[1]
+              }
+            })
+        }, (rej) => {
+          this.dialogFormVisible = false
+          console.log(rej)
+          })
+      },
+      modify() {
+        axios({
+          url: `http://127.0.0.1:5000/modifyUser/${this.form.id}`,
+          method: 'post',
+          data: {
+            username: this.form.name
+          },
+          transformRequest: [function (data) {
+            // Do whatever you want to transform the data
+            let ret = ''
+            for (let it in data) {
+              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            return ret
+          }],
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then((res) => {
+          this.dialogFormVisible = false
+          this.users = res.data.users.map((user) => {
+              return {
+                id: user[0],
+                name: user[1]
+              }
+            })
+        }, (rej) => {
+          this.dialogFormVisible = false
+          console.log(rej)
+          })
+      },
+      onSubmit() {
+        if (this.idDisabled) { // 修改
+          this.modify()
+        } else { // 添加
+          this.add()
+        }
+      },
       getUsers() {
         axios
         .get('http://127.0.0.1:5000/userList')
@@ -36,7 +188,7 @@
           this.users = res.data.users.map((user) => {
             return {
               id: user[0],
-              name: user[1]
+              name: user[1],
             }
           })
         })
@@ -47,6 +199,13 @@
     }
   }
 </script>
-
 <style>
+  .container {
+    margin: 10px;
+  }
+  .operation {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 10px;
+  }
 </style>
